a73c9d5a7e91ee276c4bab80f80ec77b36399fd8
Michael Alizade <michael.alizade@gmail.com>
2025-03-06 15:54:30 +0400

    coded the first version of the banking program, still not finished. Messed around with methods for readability and reusability. Added check balance, deposit, withdraw and exit options so far. Seems to be working fine for now, but will need further testing

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,169 @@
+import java.util.Scanner;
 
 public class Main {
 
+    static double balance = 100;
+    static int currency;
+    static int menuOption;
+
+    static int depositAmount;
+    static int withdrawAmount;
+    static double convertToEuro = 0.93;
+
+    static int interest;
+
+    static int start;
+    static int end;
+
+
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
+
+        // MIKE'S JAVA BANKING PROGRAM
+
+        System.out.println("Welcome to Mike's Java Banking Program!");
+
+
+        do{
+            System.out.print("Type 0 to start the program: ");
+            start = scanner.nextInt();
+            scanner.nextLine();
+        }while(start != 0);
+
+
+        do {
+
+            System.out.println("****************");
+            System.out.println("1. Check Balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Invest");
+            System.out.println("5. Exit");
+            menuOption = scanner.nextInt();
+
+            switch(menuOption){
+                case 1 -> checkBalance();
+                case 2 -> depositFunds();
+                case 3 -> withdrawFunds();
+                case 5 -> end = 5;
+            }
+
+        }while(end != 5);
+
+            // INVEST W/ COMPOUNDING INTEREST
+                // 3 MONTHS
+                // 6 MONTHS
+                // 1 YEAR
+                // 2 YEARS
+            // EXIT
+        // USER INPUT
+        // LOGIC
+        // EXIT MESSAGE
+
     }
+
+    // USD/EUR OPERATION CHOICE
+
+    static void chooseCurrency(){
+        System.out.println("-------------------");
+        System.out.println("Choose a currency: ");
+        System.out.println("1: USD");
+        System.out.println("2: Euro");
+        currency = scanner.nextInt();
+
+        while(currency != 1 && currency != 2){
+            System.out.println("Invalid choice! Must choose one of given options");
+            System.out.println("1: USD");
+            System.out.println("2: Euro");
+            currency = scanner.nextInt();
+        }
+    }
+
+    // CHECK BALANCE
+
+    static void checkBalance(){
+        System.out.println("-------------------");
+        System.out.println("Balance: $" + balance);
+    }
+
+    // DEPOSIT FUNDS (USD/EUR)
+
+    static void depositFunds(){
+
+        chooseCurrency();
+
+        System.out.print("Enter amount to deposit: ");
+        depositAmount = scanner.nextInt();
+        scanner.nextLine();
+
+
+            // IF USD
+        if(currency == 1){
+            System.out.println("Depositing $" + depositAmount);
+            balance += depositAmount;
+            checkBalance();
+        }
+            // IF EUR
+        else if(currency == 2){
+            System.out.println("Depositing €" + (depositAmount * convertToEuro));
+            balance += (depositAmount * convertToEuro);
+            checkBalance();
+        }
+
+    }
+
+    // WITHDRAW FUNDS (USD/EUR)
+
+    static void withdrawFunds(){
+
+        chooseCurrency();
+
+        System.out.print("Enter amount to withdraw: ");
+        withdrawAmount = scanner.nextInt();
+        scanner.nextLine();
+
+            // IF USD
+
+        if(currency == 1){
+
+            if(withdrawAmount > balance){
+                System.out.println("Insufficient funds!");
+            }
+            else if (withdrawAmount < 0){
+                System.out.println("Amount must be greater than zero!");
+            }
+            else{
+                balance -= withdrawAmount;
+                System.out.println("Withdrawing $" + withdrawAmount);
+                checkBalance();
+            }
+
+        }
+             // IF EURO
+
+        else if(currency == 2){
+
+            if(withdrawAmount * convertToEuro > balance){
+                System.out.println("Insufficient funds!");
+            }
+            else if (withdrawAmount * convertToEuro < 0){
+                System.out.println("Amount must be greater than zero!");
+            }
+            else {
+                balance -= withdrawAmount * convertToEuro;
+                System.out.println("Withdrawing €" + withdrawAmount);
+                checkBalance();
+            }
+
+
+        }
+
+
+
+
+
+    }
+
 }
